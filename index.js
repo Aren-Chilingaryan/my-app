0bec6ddc951c2dfcba172c62846b65f0c82ef373
Aren Chilingaryan <arenchilingaryan88@gmail.com>
2021-09-16 20:20:20 +0400

    changed

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function sortObjectArray(array, prop) {
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array.length - 1; j++) {
             if (array[j][prop] > array[j + 1][prop]) {
-                let x = array[j][prop];
+                const x = array[j][prop];
                 array[j][prop] = array[j + 1][prop];
                 array[j + 1][prop] = x;
             }
@@ -20,28 +20,27 @@ function sortObjectArray(array, prop) {
     }
 }
 
-function randomNumber(max, min) {
+function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-function addDiv(number, id) {
-    let newDiv = document.createElement("div");
+function addDiv(object) {
+    const newDiv = document.createElement("div");
     newDiv.style.width = "100px";
     newDiv.style.height = "30px";
     newDiv.style.margin = "30px";
     newDiv.style.border = "1px solid black";
     newDiv.style.padding = "30px";
     newDiv.style.textAlign = "center";
-    newDiv.innerText = number;
-    newDiv.id = id;
+    newDiv.innerText = object.number;
+    newDiv.id = object.id;
     document.getElementById("container").appendChild(newDiv);
-    let icon = document.createElement("img");
+    const icon = document.createElement("img");
     icon.style.position = "relative";
     icon.style.left = "60px";
     icon.style.top = "-32px";
     icon.src = "icon.2.png";
     newDiv.appendChild(icon);
-
     icon.onclick = (event) => {
         removeCard(event);
     }
@@ -52,9 +51,9 @@ function removeCard(event) {
 }
 
 function createUUID() {
-    var dt = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        var r = (dt + Math.random() * 16) % 16 | 0;
+    let dt = new Date().getTime();
+    let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        let r = (dt + Math.random() * 16) % 16 | 0;
         dt = Math.floor(dt / 16);
         return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
     });
@@ -63,7 +62,7 @@ function createUUID() {
 
 function addCard() {
     addElement();
-    addDiv(objArray[objArray.length - 1].number, objArray[objArray.length - 1].id);
+    addDiv(objArray[objArray.length - 1]);
 }
 
 function sortCards() {
@@ -79,6 +78,6 @@ function removeCards() {
 
 function draw() {
     for (let i = 0; i < objArray.length; i++) {
-        addDiv(objArray[i].number, objArray[i].id);
+        addDiv(objArray[i]);
     }
 }
